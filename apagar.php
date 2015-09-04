5908c97e4d0e04b49d15a362d930c820467b917f
Marcelo Coelho <marcelo@fidigital.com.br>
2015-09-04 11:29:15 -0300

    pw

--- apagar.php
+++ apagar.php
@@ -10,7 +10,7 @@
 <?php
 print_r($_FILES);
 $new_image_name = "namethisimage.jpg";
-move_uploaded_file($_FILES["file"]["tmp_name"], "/srv/www/upload/".$new_image_name);
+move_uploaded_file($_FILES["file"]["tmp_name"], $new_image_name);
 ?>
 
 </body>
